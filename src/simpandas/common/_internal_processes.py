41965950708354481c2d3c4688e1b914cdaa0b1f
Martín Araya <martinaraya@gmail.com>
2022-11-04 00:06:40 +0100

    separating internal helper functions

--- src/simpandas/common/_internal_processes.py
+++ src/simpandas/common/_internal_processes.py
@@ -9,6 +9,31 @@ __version__ = '0.80.6'
 __release__ = 20220927
 
 def _get_units(data, units):
+    # catch units or get from data if it is SimDataFrame or SimSeries
+    uName = None
+
+    uDict = None
+    if type(units) is dict and len(units) > 0:
+        uDict, units = units, None
+        if len(uDict) == 1:
+            if type(uDict[list(uDict.keys())[0]]) is str:
+                uName = list(uDict.keys())[0]
+                units = uDict[uName].strip()
+    elif type(units) is str and len(units.strip()) > 0:
+        self.units = units.strip()
+    elif (units is None or (type(units) is dict and len(units) > 0)) and (type(data) is SimSeries and data.units is not None):
+        if type(data.units) is str:
+            units = data.units
+            if indexUnits is None:
+                indexUnits = data.indexUnits
+                self.indexUnits = indexUnits
+        elif type(data.units) is dict:
+            units = data.units.copy()
+            if data.index.name not in units:
+                    units[data.index.name] = data.indexUnits
+    else:
+        self.units = 'unitless'
+
 
 def _get_index_atts(data=None, index=None, units=None, **kwargs):
     """
