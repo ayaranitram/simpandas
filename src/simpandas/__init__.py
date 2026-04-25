06202ac5c5a0d5618cb54915cb360819ce5f3ca1
Martín Araya <martinaraya@gmail.com>
2026-04-26 00:19:09 +0200

    updating version number

--- src/simpandas/__init__.py
+++ src/simpandas/__init__.py
@@ -5,8 +5,8 @@ Created on Sun Oct 11 11:14:32 2020
 @author: Martín Carlos Araya <martinaraya@gmail.com>
 """
 
-__version__ = '0.90.5'
-__release__ = 20260421
+__version__ = '0.90.7'
+__release__ = 20260426
 __all__ = ['SimSeries', 'SimDataFrame', 'ColumnUnits', 'read_excel', 'read_csv', 'read_json',
            'read_hdf5', 'read_summary', 'read_vdb', 'read_parquet',
            'read_prodml', 'read_witsml', 'read_resqml', 'read_schedule', 'read_auto', 'concat']
@@ -19,3 +19,4 @@ from .readers import (read_excel, read_csv, read_json, read_hdf5, read_summary,
                       read_schedule, read_auto)
 from .common.merger import concat
 from .common.units import ColumnUnits
+
